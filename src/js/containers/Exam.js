3356a03f04ec2c9774f66882dc02b9452a542807
Ivan Cheng <sschengyf@gmail.com>
2017-02-06 16:42:54 +1300

    Add placeholder for Exam and Learn pages

---
+++ src/js/containers/Exam.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Exam extends React.Component {
+	render() {
+		return (
+			<h1>Do some tests</h1>
+		);
+	}
+}
---
+++ src/js/containers/Learn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Learn extends React.Component {
+	render() {
+		return (
+			<h1>Learn some knowledge</h1>
+		);
+	}
+}
