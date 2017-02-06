629abbea6a7d03eaa1a9a13aaadd20529ce1e223
Ivan Cheng <sschengyf@gmail.com>
2017-02-06 16:42:07 +1300

    Display pages in App

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -2,12 +2,7 @@ import React from 'react';
 export default class App extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>Hello, {this.props.name}</h1>
-				<h2>Nice to meet you</h2>
-				<h3>Nice to meet you too</h3>
-				<h4>Nice to meet you too too</h4>
-			</div>
-			);
+			<div>{this.props.children}</div>
+		);
 	}
 }
