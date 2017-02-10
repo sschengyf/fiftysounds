4f995ec35190b22032ef1002991322aa8a2988b6
Ivan Cheng <sschengyf@gmail.com>
2017-02-10 23:00:17 +1300

    Adopt font awesome as icon provider

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,13 @@ module.exports = {
 			loader: 'babel-loader'
 		}, {
 			test: /\.scss$/,
-			loaders: ['style', 'css', 'postcss', 'sass']
+			loaders: ['style', 'css', 'postcss', 'resolve-url', 'sass?sourceMap']
+		}, {
+			test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "url-loader?limit=10000&minetype=application/font-woff"
+		}, {
+			test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "file-loader"
 		}]
 	},
 	devServer: {
