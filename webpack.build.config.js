d76220164036f1c096bff2fe5d746277a1b6d81d
Ivan Cheng <sschengyf@gmail.com>
2017-02-21 22:32:32 +1300

    Correct font file path for building configs

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -25,10 +25,10 @@ module.exports = {
 			loader: ExtractTextPlugin.extract('style', 'css!postcss!resolve-url!sass?sourceMap')
 		}, {
 			test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-			loader: "url-loader?limit=10000&minetype=application/font-woff"
+			loader: "url-loader?limit=10000&minetype=application/font-woff&publicPath=../&name=css/fonts/[name].[ext]"
 		}, {
 			test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-			loader: "file-loader"
+			loader: "file-loader?publicPath=../&name=css/fonts/[name].[ext]"
 		}]
 	},
 	plugins: [
