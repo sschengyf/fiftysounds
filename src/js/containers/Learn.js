7bedc138c49010ad44a0c3416446d50a4ceec299
Ivan Cheng <sschengyf@gmail.com>
2017-03-06 23:31:36 +1300

    Update a placeholder for learn alphabet page

--- src/js/containers/Learn.js
+++ src/js/containers/Learn.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class Learn extends React.Component {
 	render() {
+        const backStr = '< Go back';
+
 		return (
-			<h1>Learn some knowledge</h1>
+			<div className='learn page'>
+                <div className='learn__page__placeholder'>
+                    <h1>Coming soon</h1>
+                </div>
+                <Link to="/home" className="link learn__page__stoplink">{backStr}</Link>
+            </div>
 		);
 	}
 }
